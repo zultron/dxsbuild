8ca64962bd42894e88ea2e5ede423539a163cc41
John Morris <john@zultron.com>
2015-04-03 10:36:28 -0500

    Fix Linux package

--- scripts/package/linux.sh
+++ scripts/package/linux.sh
@@ -3,44 +3,12 @@ debug "Sourcing configs/package/linux.sh"
 VERSION=3.8.13
 RELEASE=11
 TARBALL_URL=http://www.kernel.org/pub/linux/kernel/v3.0/linux-${VERSION}.tar.xz
-DEBIAN_TARBALL=linux_$VERSION.orig.tar.xz
-DEBIAN_PKG_URL=https://github.com/zultron/linux-ipipe-deb/archive/${VERSION}.tar.gz
 GIT_URL=https://github.com/zultron/linux-ipipe-deb.git
-GIT_REPO=linux-ipipe-deb
-LOCAL_DEPS="
-    x/xenomai/xenomai-kernel-source_*.deb
-    r/rtai/rtai-source_*.deb
-    r/rtai/python-rtai_*_all.deb
-"
+DEBIAN_PACKAGE_FORMAT='3.0 (quilt)'
+DEBIAN_PACKAGE_COMP=xz
 
 FEATURESETS="xenomai rtai"
 DISABLED_FEATURESETS=""  # Set to 'xenomai' or 'rtai' to skip build
-
-#############
-# The Debian Linux package naming scheme is a small nightmare
-LINUX_SUBVER=$(echo $VERSION | sed 's/\.[0-9]*$$//')
-LINUX_PKG_ABI=1
-LINUX_PKG_EXTENSION=${LINUX_SUBVER}-${LINUX_PKG_ABI}
-
-ARCH=amd64  # FIXME:  need all arches
-
-BINARY_PACKAGES="
-    linux-support-${LINUX_PKG_EXTENSION}_${VERSION}-${RELEASE}_all.deb"
-for fs_base in $FEATURESETS; do
-    case $ARCH in
-	amd64) fs=${fs_base}.x86; flav=amd64; arch=$ARCH ;;
-	i386) fs=${fs_base}.x86; flav=686-pae; arch=$ARCH ;;
-	armhf.bbb) fs=${fs_base}.beaglebone; flav=omap; arch=armhf ;;
-	armhf.rpi) fs=${fs_base}.raspberry; flav=rpi; arch=armhf ;; # FIXME
-    esac
-    PKG_SUFF=${VERSION}-${RELEASE}_${arch}.deb
-    BINARY_PACKAGES+="
-	linux-image-${LINUX_PKG_EXTENSION}-${fs}-${flav}_${PKG_SUFF}
-	linux-headers-${LINUX_PKG_EXTENSION}-${fs}-${flav}_${PKG_SUFF}
-	linux-headers-${LINUX_PKG_EXTENSION}-common-${fs}_${PKG_SUFF}
-    "  # FIXME:  need to filter packages for each arch
-done
-#############
 
 EXTRA_BUILD_PACKAGES=python
 # Add xenomai-kernel-source if not disabled
@@ -54,12 +22,13 @@ EXTRA_BUILD_PACKAGES+=" rtai-source"
 
 configure_package() {
     (
-	cd $BUILD_DIR
-	# Disable any requested featuresets
+	cd $BUILD_SRC_DIR
 	for featureset in $DISABLED_FEATURESETS; do
+	    debug "      Disabling featureset $featureset"
 	    sed -i 's/^\( *'$featureset'$\)/#\1/' debian/config/defines
 	done
-	# Configure package
+	debug "      Running configure script"
 	debian/rules debian/control NOFAIL=true
+	debian/rules clean
     )
 }
