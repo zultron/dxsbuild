5757cc1d0024ca54b96667b8e32e3d0f4d13d84d
John Morris <john@zultron.com>
2015-04-16 14:40:55 -0500

    doit.sh:  fix rules for wheezy and rpi

--- doit.sh
+++ doit.sh
@@ -8,17 +8,43 @@ PACKAGES="dovetail-automata-keyring rtai xenomai linux linux-latest \
 
 for distro in $DISTROS; do
     for arch in $ARCHES; do
+	if test $distro = raspbian-jessie -a $arch != armhf; then
+	    # Only have armhf for rpi
+	    continue
+	fi
+
 	echo "########### dxsbuild schroot, $distro:  $arch ###########"
 	./dxsbuild -rda $arch $distro
     done
 
     for package in $PACKAGES; do
+	if test $distro != wheezy; then
+	    # except for wheezy, skip some packages
+	    case $package in
+		libsodium|zeromq3|pyzmq) continue ;;
+		jansson|libwebsockets) continue ;;
+		python-pyftpdlib) continue ;;
+	    esac
+	fi
+
+	if test $distro = raspbian-jessie; then
+	    # no kernel packages on raspbian yet
+	    case $package in
+		rtai|linux|linux-latest|linux-tools) continue ;;
+	    esac
+	fi
+
 	echo "########### dxsbuild source, $distro: $package ###########"
 	./dxsbuild -Sd $distro $package
 
 	for arch in $ARCHES; do
-	    if test $arch != amd64; then
-		# skip arch-indep packages on non-amd64
+	    if test $distro = raspbian-jessie -a $arch != armhf; then
+		# only build raspbian for armhf
+		continue
+	    fi
+
+	    if test $arch != amd64 -a $distro != raspbian-jessie; then
+		# skip arch-indep packages on non-amd64, except rpi
 		case $package in
 		    dovetail-automata-keyring) continue ;;
 		    linux-latest) continue ;;
