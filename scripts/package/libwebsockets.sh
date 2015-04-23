eaee0c35e60ab0a95974c3ea8317c412f39677e2
John Morris <john@zultron.com>
2015-04-22 19:08:37 -0500

    libwebsockets:  no cross-compile

--- scripts/package/libwebsockets.sh
+++ scripts/package/libwebsockets.sh
@@ -6,3 +6,6 @@ PACKAGE_SOURCE_URL[$PKG]="https://github.com/warmcat/libwebsockets.git"
 PACKAGE_SOURCE_GIT_BRANCH[$PKG]="$GIT_REV"
 PACKAGE_SOURCE_GIT_DEPTH[$PKG]="0"
 PACKAGE_DEBZN_GIT_URL[$PKG]="https://github.com/zultron/libwebsockets-deb.git"
+
+# Build params
+PACKAGE_NATIVE_BUILD_ONLY[$PKG]="true"		# Cross-compile broken; use qemu
