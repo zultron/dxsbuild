e7c8b0cf39e3f417a34d17ad1d4b129eddfa2c78
John Morris <john@zultron.com>
2015-04-23 13:24:44 -0500

    wheezy:  pin 'dh-python' package from backports
    In case we go this way with Machinekit

--- scripts/repo/wheezy-bpo.sh
+++ scripts/repo/wheezy-bpo.sh
@@ -9,4 +9,4 @@ REPO_MIRROR[$REPO]="http://http.debian.net/debian"
 REPO_CODENAME[$REPO]="wheezy-backports"
 
 # Package pins
-REPO_PIN_PACKAGES[$REPO]="cython cython-dbg"
+REPO_PIN_PACKAGES[$REPO]="cython cython-dbg dh-python"
