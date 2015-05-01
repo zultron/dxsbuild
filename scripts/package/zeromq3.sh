18abe9c22986229627f7537270b2783e77dadc4c
John Morris <john@zultron.com>
2015-05-01 02:37:09 -0500

    zeromq3 package:  native build only

--- scripts/package/zeromq3.sh
+++ scripts/package/zeromq3.sh
@@ -6,4 +6,5 @@ PACKAGE_SOURCE_URL[$PKG]="http://download.zeromq.org/zeromq-${VERSION}.tar.gz"
 PACKAGE_DEBZN_GIT_URL[$PKG]="https://github.com/zultron/zeromq3-deb.git"
 
 # Build params
-PACKAGE_QEMU_NOCHECK[$PKG]="true"  # Tests hang in qemu
+PACKAGE_QEMU_NOCHECK[$PKG]="true"		# Tests hang in qemu
+PACKAGE_NATIVE_BUILD_ONLY[$PKG]="true"		# Cross-compile broken; use qemu
