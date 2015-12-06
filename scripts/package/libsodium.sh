b77dc831b2ae1ca44047ea9deac47536b78bcd45
John Morris <john@zultron.com>
2015-12-06 16:00:44 -0600

    libsodium:  pull source from Debian instead of libsodium.org
    v. 1.0.0 not available upstream, but still used in jessie; pull jessie
    source for smooth apt upgrade path

--- scripts/package/libsodium.sh
+++ scripts/package/libsodium.sh
@@ -2,8 +2,15 @@ PKG="libsodium"
 VERSION="1.0.0"
 
 # Package sources
-BASE_URL="http://download.libsodium.org/libsodium/releases"
-PACKAGE_SOURCE_URL[$PKG]="$BASE_URL/libsodium-${VERSION}.tar.gz"
+#
+# 1.0.0 not available from upstream, but used in jessie; use jessie
+# source for smooth upgrade
+#
+#BASE_URL="http://download.libsodium.org/libsodium/releases"
+#PACKAGE_SOURCE_URL[$PKG]="$BASE_URL/libsodium-${VERSION}.tar.gz"
+BASE_URL="http://http.debian.net/debian/pool/main/libs/libsodium"
+PACKAGE_SOURCE_URL[$PKG]="$BASE_URL/libsodium_${VERSION}.orig.tar.gz"
+
 PACKAGE_DEBZN_GIT_URL[$PKG]="https://github.com/zultron/libsodium-deb.git"
 
 # Build params
